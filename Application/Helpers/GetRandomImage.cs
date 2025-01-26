92652036f15b3b3d0cf44327faf72566241c2ab4
Sertac10 <snaimans@gmail.com>
2025-01-26 17:52:52 +0300

    js float error fix.

---
+++ Application/Helpers/GetRandomImage.cs
@@ -0,0 +1,50 @@
+﻿using HtmlAgilityPack;
+
+namespace Application.Helpers;
+public static class GetRandomImage
+{
+    public static string uri { get; set; } = string.Empty;
+    public static async Task<string> GetRandomGoogleImage(string searchTerm)
+    {
+
+        string query = Uri.EscapeDataString(searchTerm);
+        string url = $"https://www.google.com/search?tbm=isch&q={query}";
+
+
+        var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36");
+
+        var html = await httpClient.GetStringAsync(url);
+
+
+        HtmlDocument htmlDocument = new HtmlDocument();
+        htmlDocument.LoadHtml(html);
+
+
+        var imageNodes = htmlDocument.DocumentNode.SelectNodes("//img")
+                            .Where(node => node.Attributes["src"] != null)
+                            .Select(node => node.Attributes["src"].Value)
+                            .ToList();
+
+
+        var firstFiveImages = imageNodes.Take(5).ToList();
+
+        if (firstFiveImages.Count > 0)
+        {
+
+            Random random = new Random();
+            int randomIndex = random.Next(firstFiveImages.Count);
+            return firstFiveImages[randomIndex];
+        }
+        else
+        {
+            return "No image found";
+        }
+    }
+
+
+    static GetRandomImage()
+    {
+        uri = GetRandomGoogleImage("rick").GetAwaiter().GetResult();
+    }
+}
--- RickAndMortyBlazorV1/wwwroot/js/myjs.js
+++ RickAndMortyBlazorV1/wwwroot/js/myjs.js
@@ -21,13 +21,20 @@ function closeOnClickOutside(modalId, dotNetObjRef) {
 
 
 
-
 window.scrollHandler = function (dotNetHelper) {
     window.onscroll = function () {
-        let scrollPosition = window.scrollY || document.documentElement.scrollTop;
-        dotNetHelper.invokeMethodAsync('HandleScroll', scrollPosition);
+        let scrollPosition = Math.min(
+           
+            parseInt(window.scrollY)
+        );
+
+        if (dotNetHelper && dotNetHelper.invokeMethodAsync) {
+            dotNetHelper.invokeMethodAsync('HandleScroll', scrollPosition)
+                .catch(error => console.error("Blazor calling method error: ", error));
+        }
     };
 };
+
 
 window.changeTheme = (themeName) => {
     var link = document.getElementById('theme-link');
@@ -35,5 +42,74 @@ window.changeTheme = (themeName) => {
         link.href = `css/theme/${themeName}.css`;
        
     }
-    /*location.reload();*/
+    
 };
+
+//async function getRandomGoogleImage(searchTerm) {
+//    const query = encodeURIComponent(searchTerm);
+//    const url = `https://www.google.com/search?tbm=isch&q=${query}`;
+
+//    try {
+//        const response = await fetch(url, {
+//            method: 'GET',
+//            headers: {
+//                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+//                'Access-Control-Allow-Origin': '*'
+//            },
+//            mode: 'no-cors'
+//        });
+
+//        const html = await response.text();
+
+//        const parser = new DOMParser();
+//        const doc = parser.parseFromString(html, 'text/html');
+
+//        const imgTags = Array.from(doc.querySelectorAll('img'));
+
+//        const firstFiveImages = imgTags.slice(0, 5).map(img => img.src);
+
+//        if (firstFiveImages.length > 0) {
+//            const randomIndex = Math.floor(Math.random() * firstFiveImages.length);
+//            return firstFiveImages[randomIndex];
+//        } else {
+//            return "No image found";
+//        }
+//    } catch (error) {
+//        console.error('Error fetching images:', error);
+//        return "Error";
+//    }
+//}
+
+//async function getRandomBingImage(searchTerm) {
+//    const query = encodeURIComponent(searchTerm);
+//    const url = `https://www.bing.com/images/search?q=${query}`;
+
+//    const response = await fetch(url, {
+//        headers: {
+//            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
+//            "Accept-Language": "en-US,en;q=0.5"
+//        },
+//        mode: 'no-cors'
+//    });
+
+//    const htmlText = await response.text();
+//    const parser = new DOMParser();
+//    const doc = parser.parseFromString(htmlText, "text/html");
+
+//    // img taglerini ve resim URL'lerini alıyoruz
+//    const imageNodes = Array.from(doc.querySelectorAll("img.mimg")).map(img => img.src);
+
+//    // İlk 5 resmi alıyoruz
+//    const firstFiveImages = imageNodes.slice(0, 5);
+
+//    if (firstFiveImages.length > 0) {
+//        const randomIndex = Math.floor(Math.random() * firstFiveImages.length);
+//        return firstFiveImages[randomIndex]; // Rastgele bir resim seçiyoruz
+//    } else {
+//        return "No image found";
+//    }
+//}
+
+
+
+
